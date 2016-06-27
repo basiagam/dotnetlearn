1b8087e2dde8c961753254c5337537d8325f17bb
Marcin Basiaga <basiagam@yahoo.com>
2016-06-27 21:29:02 +0200

    modele

---
+++ MVCReflectionModel/MVCReflectionModel/Models/AssemblyModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+
+namespace MVCReflectionModel.Models
+{
+    public class AssemblyModel
+    {
+        private readonly Assembly _asm;
+        
+        public AssemblyModel(Assembly asm)
+        {
+            _asm = asm;
+            AssemblyName name = asm.GetName();
+            SimpleName = name.Name;
+            Version = name.Version.ToString();
+            byte[] keyToken = name.GetPublicKeyToken();
+            PublicKeyToken = keyToken == null ? "" :
+                string.Concat(keyToken.Select(b => b.ToString("X2")));
+            Types = asm.GetTypes().Select(t => t.FullName).ToList();
+
+        }
+        public IList<string> Types { get; private set; }
+        public string PublicKeyToken { get; private set; }
+        public string SimpleName { get; private set; }
+
+        public string Version { get; private set; }
+    }
+}
