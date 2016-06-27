297d5c7454c7918b4fd2ba44ffdafcb7b8da536e
Marcin Basiaga <basiagam@yahoo.com>
2016-06-27 21:12:05 +0200

    mvc start

---
+++ MVCReflectionModel/MVCReflectionModel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCReflectionModel.Startup))]
+namespace MVCReflectionModel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
