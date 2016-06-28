2dfc8ae9d08541d2534f3872a96ffaacfad72fe7
Marcin Basiaga <basiagam@yahoo.com>
2016-06-28 13:41:35 +0200

    mv

---
+++ MVCReflectionModel/MVCReflectionModel/Controllers/ReflectionController.cs
@@ -0,0 +1,23 @@
+﻿using MVCReflectionModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCReflectionModel.Controllers
+{
+    public class ReflectionController : Controller
+    {
+        // GET: Reflection
+        public ActionResult Assembly(string id)
+        {
+            AssemblyModel model = ModelSource.FromName(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+    }
+}
