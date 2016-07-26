3e1dc87443c4f186f7315705f6252b5043f2d9c4
Marcin Basiaga <basiagam@yahoo.com>
2016-07-26 09:37:25 +0200

    view ienumerable

--- MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -12,7 +12,13 @@ namespace MvcMusicStore.Controllers
         // GET: Store
         public ActionResult Index()
         {
-            return View();
+            var genres = new List<Genre>
+            {
+                new Genre { Name = "Disco"},
+                new Genre { Name = "Jazz"},
+                new Genre { Name = "Rock" }
+            };
+            return View(genres);
         }
         // GET: Store/Browse
         public ActionResult Browse(string genre)
