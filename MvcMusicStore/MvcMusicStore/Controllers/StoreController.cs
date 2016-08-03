5c2c5c868b62f892b0782ee3b5d47c23b8f33c43
Marcin Basiaga <basiagam@yahoo.com>
2016-08-03 21:05:13 +0200

    a

--- MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -12,13 +12,19 @@ namespace MvcMusicStore.Controllers
         // GET: Store
         public ActionResult Index()
         {
-            return View();
+            var genres = new List<Genre>
+            {
+                 new Genre { Name = "Disco"},
+                new Genre { Name = "Jazz"},
+                new Genre { Name = "Rock"}
+            };
+            return View(genres);
         }
         // GET: Store/Browse
         public ActionResult Browse(string genre)
         {
             var genreModel = new Genre { Name = genre };
-            return View(genreModel)
+            return View(genreModel);
         }
         // GET: Store/Details
         public ActionResult Details(int id)
---
+++ MvcMusicStore/MvcMusicStore/Models/MusicStoreEntities.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MvcMusicStore.Models
+{
+    public class MusicStoreEmusicntities : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
