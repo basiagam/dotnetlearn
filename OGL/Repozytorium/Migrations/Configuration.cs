fb25b828dbf3385867a86ecac25b201487083ea1
Marcin Basiaga <basiagam@yahoo.com>
2016-08-11 14:29:55 +0200

    seed

--- OGL/Repozytorium/Migrations/Configuration.cs
+++ OGL/Repozytorium/Migrations/Configuration.cs
@@ -1,5 +1,7 @@
 namespace Repozytorium.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using Models;
     using System;
     using System.Data.Entity;
@@ -12,6 +14,11 @@ namespace Repozytorium.Migrations
         {
             AutomaticMigrationsEnabled = false;
             AutomaticMigrationDataLossAllowed = true;
+
+            // Do debugowania metody seed
+            // if (System.Diagnostics.Debugger.IsAttached == false)
+            // System.Diagnostics.Debugger.Launch();
+            
         }
 
         protected override void Seed(OglContext context)
@@ -28,6 +35,89 @@ namespace Repozytorium.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+            SeedRoles(context);
+            SeedUsers(context);
+            SeedOgloszenia(context);
+            SeedKategorie(context);
+            SeedOgloszenie_Kategoria(context);
         }
+
+        private void SeedRoles(OglContext context)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>());
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role =new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+            }
+        }
+        private void SeedUsers(OglContext context)
+        {
+            var store = new UserStore<Uzytkownik>(context);
+            var manager = new UserManager<Uzytkownik>(store);
+            if(!context.Users.Any(u=>u.UserName == "Admin"))
+            {
+                var user = new Uzytkownik { UserName = "Admin" };
+                var adminresult = manager.Create(user, "12345678");
+                if (adminresult.Succeeded)
+                    manager.AddToRole(user.Id, "Admin");
+            }
+        }
+
+        private void SeedOgloszenia(OglContext context)
+        {
+            var idUzytkownika = context.Set<Uzytkownik>().Where(u => u.UserName ==
+            "Admin").FirstOrDefault().Id;
+            for (int i = 1; i <= 10; i++)
+            {
+                var ogl = new Ogloszenie()
+                {
+                    Id = i,
+                    UzytkownikId = idUzytkownika,
+                    Tresc = "Treœæ og³oszenia" + i.ToString(),
+                    Tytul = "Tytu³ og³oszenia" + i.ToString(),
+                    DataDodania = DateTime.Now.AddDays(-i)
+                };
+                context.Set<Ogloszenie>().AddOrUpdate(ogl);
+            }
+            context.SaveChanges();
+        }
+
+        private void SeedKategorie(OglContext context)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                var kat = new Kategoria()
+                {
+                    Id = i,
+                    Nazwa = "Nazwa kategorii" + i.ToString(),
+                    Tresc = "Treœæ og³oszenia" + i.ToString(),
+                    MetaTytul = "Tytu³ kategorii" + i.ToString(),
+                    MetaOpis = "Opis kategorii" + i.ToString(),
+                    MetaSlowa = "S³owa kluczowe do kategorii" + i.ToString(),
+                    ParentId = i
+                };
+                context.Set<Kategoria>().AddOrUpdate(kat);
+            }
+            context.SaveChanges();
+        }
+        private void SeedOgloszenie_Kategoria(OglContext context)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                var okat = new Ogloszenie_Kategoria()
+                {
+                    Id = i,
+                    OgloszenieId = i / 2 + 1,
+                    KategoriaId = i / 2 + 2
+                };
+                context.Set<Ogloszenie_Kategoria>().AddOrUpdate(okat);
+            }
+            context.SaveChanges();
+        }
+
+
     }
 }
