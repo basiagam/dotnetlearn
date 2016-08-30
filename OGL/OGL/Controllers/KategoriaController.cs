2a584dee03893d9fce413c8bd2a0ea1a1aed1d0b
Marcin Basiaga <basiagam@yahoo.com>
2016-08-30 15:37:16 +0200

    kategoria controller

---
+++ OGL/OGL/Controllers/KategoriaController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Repozytorium.Models;
+using Repozytorium.IRepo;
+
+namespace OGL.Controllers
+{
+    public class KategoriaController : Controller
+    {
+        private readonly IKategoriaRepo _repo;
+
+        public KategoriaController(IKategoriaRepo repo)
+        {
+            _repo = repo;
+        }
+
+        // GET: Kategoria
+        public ActionResult Index()
+        {
+            var kategorie = _repo.PobierzKategorie().AsNoTracking();
+            return View(kategorie);
+        }
+
+        public ActionResult PokazOgloszenia(int id)
+        {
+            var ogloszenia = _repo.PobierzOgloszeniaZKategorii(id);
+            return View(ogloszenia);
+        }
+
+    }
+}
---
+++ OGL/Repozytorium/IRepo/IKategoriaRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Repozytorium.IRepo
+{
+    public interface IKategoriaRepo
+    {
+        IQueryable<Kategoria> PobierzKategorie();
+        object PobierzOgloszeniaZKategorii(int id);
+    }
+}
