8a4ad2a7b7810a7fa4efb48d1484df09d7dfe725
Marcin Basiaga <basiagam@yahoo.com>
2016-10-19 21:20:10 +0200

    material

---
+++ MaterialC/MaterialC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MaterialC.Startup))]
+namespace MaterialC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
